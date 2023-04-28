42192164a9e8e70450cbb3831bd7e1bd3b3db129
Osvaldo Gago <opgago@yahoo.com>
2023-04-28 22:36:51 +0100

    Example rule

--- rules.js
+++ rules.js
@@ -1,4 +1,12 @@
 /* jshint esversion: 8 */
+
 dataLayer.rule(function (params) {
-    console.log("Params are: ", params);
+    if (
+        params
+    ) {
+        console.log("Params are: ", params);
+        dataLayer.logs.push("Comment added to the log");
+    } else {
+        dataLayer.logs.push("Another comment added to the log");
+    }
 });
