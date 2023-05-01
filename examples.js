0770301aecbcca074b598aeee0bfea39b42f273b
Osvaldo Gago <opgago@yahoo.com>
2023-05-01 19:16:33 +0100

    Rename to myTM
    Signed-off-by: Osvaldo Gago <opgago@yahoo.com>

--- examples.js
+++ examples.js
@@ -1,64 +1,64 @@
 /* jshint esversion: 8 */
 
-const myTagManager = Object.create(tagManager);
+const myTM = Object.create(tagManager);
 
 tagManager.debug = true;
 
-myTagManager.accounts = {};
+myTM.accounts = {};
 
 /*
- * TAGS (Tags and myTagManager conditions of when they are fired)
- * The name of the function is for reference only with myTagManager.tags
+ * TAGS (Tags and myTM conditions of when they are fired)
+ * The name of the function is for reference only with myTM.tags
  * Inside the "if" condition area put the conditions which the tag fires,
- * it can include myTagManagerParams or variables
+ * it can include myTMParams or variables
  * Inside the "if" procedure area put the javascript tracking tag
 */
 
-myTagManager.tag(function once_per_page_example(params) {
+myTM.tag(function once_per_page_example(params) {
     if (
         params.event === "DOM ready"
     ) {
-        myTagManager.logs.push("Trigger to run on Dom Ready");
+        myTM.logs.push("Trigger to run on Dom Ready");
     }
 });
 
-myTagManager.tag(function once_per_page_2_example(params) {
+myTM.tag(function once_per_page_2_example(params) {
     if (
         params.event === "DOM ready"
     ) {
-        myTagManager.logs.push("Second trigger to run on Dom Ready");
+        myTM.logs.push("Second trigger to run on Dom Ready");
     }
 });
 
-myTagManager.tag(function click_li_example(params) {
+myTM.tag(function click_li_example(params) {
     if (
         params.event === "click" && params.element === "li"
     ) {
-        myTagManager.logs.push("Recorded click event in li");
+        myTM.logs.push("Recorded click event in li");
     }
 });
 
 /*
- * TRIGGERS (Tags and myTagManager conditions of when they are fired)
+ * TRIGGERS (Tags and myTM conditions of when they are fired)
  * First parameter: Name of the DOM events
  * Second parameter: CSS selector of the element, use "" for none
- * Third parameter: Object with the myTagManager push elements.
+ * Third parameter: Object with the myTM push elements.
 */
 
 // Example click event in a <li> html element
-myTagManager.trigger("click", "li", {
+myTM.trigger("click", "li", {
     "event": "click",
     "element": "li"
 });
 
 // Example click event with custom function
-myTagManager.trigger("click", "p.manual", function (e) {
+myTM.trigger("click", "p.manual", function (e) {
     dataLayer.push({
         "event": "click",
         "target": e.target,
         "text": e.target.innerText
     });
-    myTagManager.logs.push("Recorded " + e.type + " event");
+    myTM.logs.push("Recorded " + e.type + " event");
 });
 
 // HTML example, 10 seconds on page
