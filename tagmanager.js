de74658b7474d45dced323106af621699f34fd83
Osvaldo Gago <opgago@yahoo.com>
2023-05-13 08:08:39 +0100

    Add cookie variables
    Signed-off-by: Osvaldo Gago <opgago@yahoo.com>

--- tagmanager.js
+++ tagmanager.js
@@ -50,7 +50,23 @@ tagManager.variables = {
         day: new Date().getDate(),
         weekday: new Date().getDay(),
         hour: new Date().getHours(),
-        minute: new Date().getMinutes()
+        minute: new Date().getMinutes(),
+        timezoneOffset: new Date().getTimezoneOffset() / 60,
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    },
+    getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+
+    },
+    setCookie(name, value, exp = 5) {
+        const date = new Date();
+        date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+        document.cookie = `${name}=${value}; Path=/; SameSite=Lax; expires=${date.toUTCString()}`;
+    },
+    deleteCookie(name) {
+        document.cookie = `${name}=; Path=/; SameSite=Lax; expires=Thu, 01 Jan 1999 00:00:10 GMT;`;
     }
 };
 
